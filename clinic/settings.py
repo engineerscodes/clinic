74ab9ba03652cd724ecdcb5cba83d6e6201e625b
jayanpraveen <jayanpraveen30@gmail.com>
2021-05-26 16:00:49 +0530

    updated static files

--- clinic/settings.py
+++ clinic/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z7fiei8q1!#mmgd=5*@y0di0w0tmwh3%kj#&lu3a-34xz2ppa@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
